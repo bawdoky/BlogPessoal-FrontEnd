dd4dc91639195541b6c80015e750a82c2ca475e5
Lucas Oi <lucas.oi-@hotmail.com>
2022-02-16 17:41:25 -0300

    Conclusão da atv react3 e revisão M-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import {Grid} from '@material-ui/core'
 import Home from './paginas/home/Home';
 import './App.css';
 
--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import IntagramIcon from '@material-ui/icons/Instagram';
+import LinkedinIcon from '@material-ui/icons/LinkedIn';
+import {Typography, Box, Grid} from '@material-ui/core';
 
-function Footer (){
+function Footer() {
     return (
         <>
-            <h1>Footer</h1>
+            <Grid container direction="row" justifyContent="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.facebook.com/" target="blank">
+                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://www.instagram.com/" target="blank">
+                                <IntagramIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://www.linkedin.com/" target="blank">
+                                <LinkedinIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </Box>
+                    </Box >
+                </Grid>
+            </Grid>
         </>
     )
 }

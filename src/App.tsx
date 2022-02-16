44c82130b12ce0c1656786b24ff3ef36eeabd8c1
Lucas Oi <lucas.oi-@hotmail.com>
2022-02-16 00:08:01 -0300

    Ajuste de pastas e inclusão react3

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import {Grid} from '@material-ui/core'
+import Home from './paginas/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button, Box, Paper} from '@material-ui/core'
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1>Home</h1>
+        </>
+    );
+}
+
+export default Home;

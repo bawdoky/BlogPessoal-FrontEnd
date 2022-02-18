6fee6e9a16558345308f124e1a569eec8dbe54f8
Lucas Oi <lucas.oi-@hotmail.com>
2022-02-18 00:05:54 -0300

    atividade react 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
-import './App.css';
-
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
+          <Route exact path='/'>
+              <Login />
+            </Route>
+
+          <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+          </div>
+        </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
-
 export default App;

5b05a7cb28d9523e95b75447d1cf554f58f5f544
Lucas Oi <lucas.oi-@hotmail.com>
2022-02-18 17:03:33 -0300

    Correção do NavBar com Links

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -15,26 +15,38 @@ function Navbar() {
                     </Box>
 
                     <Box display="flex" justifyContent="start">
+                        <Link to='/home' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 home
                             </Typography>
                         </Box>
+                        </Link>
+
+                        <Link to='/postagens' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 postagens
                             </Typography>
                         </Box>
+                        </Link>
+                        
+                        <Link to='/temas' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 temas
                             </Typography>
                         </Box>
+                        </Link>
+
+                        <Link to='/cadastratemas' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 cadastrar temas
                             </Typography>
                         </Box>
+                        </Link>
+                    
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">

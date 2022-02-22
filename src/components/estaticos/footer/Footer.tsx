cd1e864be3024ce2ad453884703a91f0faa43caa
Lucas Oi <lucas.oi-@hotmail.com>
2022-02-22 08:40:15 -0300

    Atv react5, inclusão models e services

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import IntagramIcon from '@material-ui/icons/Instagram';
 import LinkedinIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Box, Grid} from '@material-ui/core';
+import { Typography, Box, Grid } from '@material-ui/core';
 import './Footer.css';
 
 function Footer() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center">
-                <Grid alignItems="center" item xs={12}>
+            <Grid container direction='row' justifyContent='center'>
+                <Grid alignItems='center' item xs={12}>
                     <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                        <Box paddingTop={1} display='flex' alignItems='center' justifyContent='center'>
+                            <Typography variant='h5' align='center' gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/" target="blank">
+                        <Box display='flex' alignItems='center' justifyContent='center'>
+                            <a href="https://www.facebook.com/" target='blank'>
                                 <FacebookIcon className='redes' />
                             </a>
-                            <a href="https://www.instagram.com/" target="blank">
+                            <a href="https://www.instagram.com/" target='blank'>
                                 <IntagramIcon className='redes' />
                             </a>
-                            <a href="https://www.linkedin.com/" target="blank">
+                            <a href="https://www.linkedin.com/" target='blank'>
                                 <LinkedinIcon className='redes' />
                             </a>
                         </Box>
@@ -31,8 +31,8 @@ function Footer() {
                             <Typography variant='subtitle2' align='center' gutterBottom className='textos'> 2022 Copyringht</Typography>
                         </Box>
                         <Box>
-                            <a target= "blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='textos' align='center'> brasil.generation</Typography>
+                            <a target='blank' href="https://brasil.generation.org">
+                                <Typography variant='subtitle2' gutterBottom className='textos' align='center'> brasil.generation</Typography>
                             </a>
                         </Box>
                     </Box>
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -6,50 +6,50 @@ import './Navbar.css';
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
+            <AppBar position='static'>
+                <Toolbar variant='dense'>
                     <Box className='cursor'>
-                        <Typography variant="h5" color="inherit">
+                        <Typography variant='h5' color='inherit'>
                             BlogPessoal
                         </Typography>
                     </Box>
 
-                    <Box display="flex" justifyContent="start">
+                    <Box display='flex' justifyContent='start'>
                         <Link to='/home' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant='h6' color='inherit'>
+                                    home
+                                </Typography>
+                            </Box>
                         </Link>
 
                         <Link to='/postagens' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant='h6' color='inherit'>
+                                    postagens
+                                </Typography>
+                            </Box>
                         </Link>
-                        
+
                         <Link to='/temas' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
                         </Link>
 
                         <Link to='/cadastratemas' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar temas
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant='h6' color='inherit'>
+                                    cadastrar temas
+                                </Typography>
+                            </Box>
                         </Link>
-                    
+
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
+                                <Typography variant='h6' color='inherit'>
                                     logout
                                 </Typography>
                             </Box>
